49598346f4a2807bc4ef18c5b7016f65a577dbae
Alessio Masucci <alessio.masucci@immobiliare.it>
2021-01-05 18:35:51 +0100

    Adds Credits in template

--- components/Card.vue
+++ components/Card.vue
@@ -2,7 +2,8 @@
   <nuxt-link :to="`work/${work.slug}`" class="relative flex flex-col">
     <div class="shadow-xl pt-media-sm md:pt-media-md lg:pt-media-lg">
       <img
-        :src="work.cover"
+        v-if="work.cover && work.cover.image"
+        :src="work.cover.image"
         alt=""
         class="absolute inset-0 w-full h-full object-cover bg-gray-100"
       />
@@ -10,9 +11,7 @@
     <div class="absolute bottom-0 left-0 w-full">
       <div class="relative z-10 px-4 pt-40 pb-3 bg-gradient-to-t from-black">
         <p class="text-sm font-medium text-white">{{ work.project_type }}</p>
-        <h2
-          class="text-xl font-semibold text-white sm:text-2xl sm:leading-7 md:text-3xl"
-        >
+        <h2 class="text-xl font-semibold text-white sm:text-2xl md:text-3xl">
           {{ work.title }}
         </h2>
       </div>
