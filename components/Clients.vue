cdd44a20dbf8633352947adbed3d264d4f984311
Alessio Masucci <alessio.masucci@immobiliare.it>
2021-01-19 20:49:23 +0100

    Changes clients grid

--- components/Clients.vue
+++ components/Clients.vue
@@ -1,14 +1,18 @@
 <template>
   <section class="bg-gray-200 py-8">
-    <div class="container mx-auto">
+    <div class="p-4 container mx-auto max-w-screen-lg">
       <p class="text-2xl font-semibold">They have already trusted me.</p>
-      <Gallery :columns="6">
-        <GalleryItem v-for="(client, i) in clients" :key="i">
-          <figure class="pt-ful">
-            <img :src="client.logo" :alt="client.name" />
+      <div class="grid gap-4 grid-cols-3 sm:grid-cols-4 md:grid-cols-6 mt-16">
+        <div v-for="(client, i) in clients" :key="i">
+          <figure class="relative pt-full">
+            <img
+              class="absolute inset-0 w-full h-full"
+              :src="client.logo"
+              :alt="client.name"
+            />
           </figure>
-        </GalleryItem>
-      </Gallery>
+        </div>
+      </div>
     </div>
   </section>
 </template>
